3268c7f74e11e31d84ade48c87f00dc4f4d4ec5b
Paul Portier <paulportier@gmail.com>
2020-04-22 22:58:03 +0200

    scroll and logo

--- app/javascript/components/mousewheel_actions.js
+++ app/javascript/components/mousewheel_actions.js
@@ -1,13 +1,42 @@
 const mouseWheelActions = () => {
 
+  const contactPage = document.querySelector('#contact-page');
+  const galleryPage = document.querySelector('#gallery-page');
+  const infoPage = document.querySelector('#info-page');
+
+  const contactOpen = document.querySelector('#contact-link')
+  const galleryOpen = document.querySelector('#gallery-link')
+  const infoOpen = document.querySelector('#info-link')
+
+  const closeContact = document.querySelector('#close-contact-arrow');
+  const closeGallery = document.querySelector('#close-gallery-arrow');
+  const closeInfo = document.querySelector('#close-info-arrow');
+
   window.addEventListener('wheel', (event) => {
-    if (event.deltaX < -50 ) {
+    if (event.deltaX < -50) {
+    // If we scroll left
+
       document.querySelector('#contact-link').click();
+
+
     } else if (event.deltaX > 50) {
+    // If we scroll right
+
+
       document.querySelector('#gallery-link').click();
+
+
+    } else if (event.deltaY > 50) {
+    // If we scroll down
+
+
+
+    } else if (event.deltaY < -50) {
+    // If we scroll up
+
     }
+
   });
 };
 
 export { mouseWheelActions };
-
