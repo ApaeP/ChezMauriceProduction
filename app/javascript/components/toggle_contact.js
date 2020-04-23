8f9c0196727ba451dae62a35c937ead44c13b09f
Paul Portier <paulportier@gmail.com>
2020-04-23 17:46:04 +0200

    authorize scroll on dedicated elements only without js

--- app/javascript/components/toggle_contact.js
+++ app/javascript/components/toggle_contact.js
@@ -6,32 +6,14 @@ const contactActions = () => {
   // When contact link is clicked
   contactButton.addEventListener('click', (event) => {
     // Set "#contact-page" css paramater "top" to the right value (which is the height of the #contact-page div)
-    contactPage.style.top = `${contactPage.offsetHeight}px`;
+    // contactPage.style.top = `${contactPage.offsetHeight}px`;
+    // We'd rather set the top to the height of the window
+    contactPage.style.top = `${window.innerHeight}px`;
     // display the contact-page div
     if (contactPage.classList.contains('hidden')) {
       contactPage.classList.remove('hidden');
     }
     contactPage.classList.add('displayed');
-    // allow scroll on Y axis for contact-page
-    document.querySelector("body").style.overflowY = 'scroll';
-
-      // document.querySelector('body').addEventListener('wheel', (event) => {
-      //   let maxScroll = contactPage.offsetHeight - window.innerHeight
-      //   // console.log(`Hauteur contact height = ${maxScroll}`);
-      //   // console.log(`height of window = ${window.innerHeight}`)
-      //   // console.log(`distance scrolled = ${window.pageYOffset}`);
-      //   if (window.pageYOffset > maxScroll && event.deltaY > 1) {
-      //     window.pageYOffset = maxScroll;
-      //     document.querySelector("body").style.overflowY = 'hidden';
-      //   }
-      // });
-      // // onScroll = function (e){
-      // //    var maxScroll=1200
-      // //    if(e.target.scrollLeft>maxScrollLeft){
-      // //      e.target.scrollLeft=maxScrollLeft
-      // //    }
-      // // }
-
 
     // pause on video background after transition is over
     setTimeout('document.querySelector("#home-video").pause()', 1000)
