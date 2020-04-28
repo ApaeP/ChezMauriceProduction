1771ba516b272008202aa5dd3fdb51aed3ec03c4
Paul Portier <paulportier@gmail.com>
2020-04-28 15:20:44 +0200

    Revert "fix bug vimeo"
    This reverts commit 7370431527fec8b0af3b90e7e2f54ac3951eb7d5.

--- app/controllers/vimeovideos_controller.rb
+++ app/controllers/vimeovideos_controller.rb
@@ -2,14 +2,14 @@ class VimeovideosController < ApplicationController
   vimeo = VimeoMe2::VimeoObject.new(ENV['VIMEO_ACCESS_TOKEN'])
     # @vimeo_video = VimeoMe2::Video.new(ENV['VIMEO_ACCESS_TOKEN'], '380593500')
     # @vimeo_video_html = @vimeo_video.video["embed"]["html"]
-  # def create
-  #   @vimeovideo = VimeoMe2::Video.new(ENV['VIMEO_ACCESS_TOKEN'], video_params)
-  #   if @video.save!
-  #     p "\n \n \n SAVED\n \n \n "
-  #   else
-  #     p "\n \n \n NOT SAVED \n \n \n"
-  #   end
-  # end
+  def create
+    @vimeovideo = VimeoMe2::Video.new(ENV['VIMEO_ACCESS_TOKEN'], video_params)
+    if @video.save!
+      p "\n \n \n SAVED\n \n \n "
+    else
+      p "\n \n \n NOT SAVED \n \n \n"
+    end
+  end
 
   private
 
