0db2a79b31f64f8ad066e8a336c778454c67c43d
Paul Portier <paulportier@gmail.com>
2020-05-07 05:58:24 +0200

    cors in app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,26 +6,26 @@ class ApplicationController < ActionController::Base
 
   # # For all responses in this controller, return the CORS access control headers.
 
-  # def cors_set_access_control_headers
-  #   headers['Access-Control-Allow-Origin'] = '*'
-  #   headers['Access-Control-Allow-Methods'] = 'POST, PUT, GET, OPTIONS'
-  #   headers['Access-Control-Request-Method'] = '*'
-  #   headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-  # end
+  def cors_set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, PUT, GET, OPTIONS'
+    headers['Access-Control-Request-Method'] = '*'
+    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  end
 
   # # If this is a preflight OPTIONS request, then short-circuit the
   # # request, return only the necessary headers and return an empty
   # # text/plain.
 
-  # def cors_preflight_check
-  #   if request.method == :options
-  #     headers['Access-Control-Allow-Origin'] = '*'
-  #     headers['Access-Control-Allow-Methods'] = 'POST, PUT, GET, OPTIONS'
-  #     headers['Access-Control-Request-Method'] = '*'
-  #     headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-  #     render :text => '', :content_type => 'text/plain'
-  #   end
-  # end
+  def cors_preflight_check
+    if request.method == :options
+      headers['Access-Control-Allow-Origin'] = '*'
+      headers['Access-Control-Allow-Methods'] = 'POST, PUT, GET, OPTIONS'
+      headers['Access-Control-Request-Method'] = '*'
+      headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+      render :text => '', :content_type => 'text/plain'
+    end
+  end
   def default_url_options
     { host: ENV["DOMAIN"] || "localhost:3000" }
   end
