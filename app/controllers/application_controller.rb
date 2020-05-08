ae16d1745de62f8869e866ca31e67ebe819259ab
Paul Portier <paulportier@gmail.com>
2020-05-08 15:01:26 +0200

    oui

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :first_time_visit, unless: -> { cookies[:first_visit] }
+  # before_action :first_time_visit, unless: -> { cookies[:first_visit] }
   # before_action :authenticate_user!
   # before_action :cors_set_access_control_headers
 
