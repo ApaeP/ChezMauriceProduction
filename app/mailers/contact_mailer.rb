4a680579de85f381838ddf5627aa65397542931b
Paul Portier <paulportier@gmail.com>
2020-06-12 12:59:05 +0200

    mail config

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -2,12 +2,16 @@ class ContactMailer < ApplicationMailer
   def mail_to_client
     # @contact = Contact.new
     @contact = params[:contact]
-    mail(to: @contact.email, subject: 'Chez Maurice Production | Votre demande de contact')
+    email_with_name = %("#{@contact.name}" <#{@contact.email}>)
+    sender_with_name = %("Chez Maurice Production | Contact" <#{ENV['CONTACT_EMAIL']}>)
+    mail(to: email_with_name, from: sender_with_name, subject: 'Chez Maurice Production | Votre demande de contact')
   end
 
   def mail_to_prod
     # @contact = Contact.new
     @contact = params[:contact]
-    mail(to: ENV['CONTACT_EMAIL'], subject: "Chez Maurice Production | Demande de contact de la part de #{@contact.name} (n°#{@contact.id})")
+    email_with_name = "Pierre-Alexandre <#{ENV['OWNER_EMAIL']}>"
+    sender_with_name = %("Chez Maurice Production | Contact" <#{ENV['CONTACT_EMAIL']}>)
+    mail(to: email_with_name, from: sender_with_name, subject: "Chez Maurice Production | Demande de contact de la part de #{@contact.name} (n°#{@contact.id})")
   end
 end
