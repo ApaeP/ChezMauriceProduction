918557a5d35b4739c033ac5ce2f99eca279bc052
Paul Portier <paulportier@gmail.com>
2020-06-23 19:15:36 +0200

    email layouts

--- test/mailers/previews/contact_mailer_preview.rb
+++ test/mailers/previews/contact_mailer_preview.rb
@@ -1,4 +1,14 @@
 # Preview all emails at http://localhost:3000/rails/mailers/contact_mailer
 class ContactMailerPreview < ActionMailer::Preview
+  def mail_to_client
+    # @contact = Contact.new
+    @contact = Contact.new(name: 'Robert Bite', email: 'robertbite@test.com', content: 'Bonjour, belle bite, le bout est à un coup de bagnole.')
+    ContactMailer.with(contact: @contact).mail_to_client
+  end
 
+  def mail_to_prod
+    # @contact = Contact.new
+    @contact = Contact.new(name: 'Robert Bite', email: 'robertbite@test.com', content: 'Bonjour, belle bite, le bout est à un coup de bagnole.')
+    ContactMailer.with(contact: @contact).mail_to_prod
+  end
 end
