93175302c568d640e0836f2e33e307947d36d749
Paul Portier <paulportier@gmail.com>
2020-07-03 18:23:09 +0200

    correction de la hauteur du login container

--- app/javascript/components/set_main_container_minheight.js
+++ app/javascript/components/set_main_container_minheight.js
@@ -1,6 +1,10 @@
 const setMainContainerMinHeight = () => {
   if (document.querySelector('.main-container')) {
-    document.querySelector('.main-container').style.minHeight = window.innerHeight - ( document.querySelector('.navbar-container').offsetHeight + document.querySelector('.footer-lrg').offsetHeight ) + 'px';
+    if (document.querySelector('.log-in-container')) {
+      document.querySelector('.main-container').style.height = window.innerHeight - ( document.querySelector('.navbar-container').offsetHeight + document.querySelector('.footer-lrg').offsetHeight ) + 'px';
+    } else {
+      document.querySelector('.main-container').style.minHeight = window.innerHeight - ( document.querySelector('.navbar-container').offsetHeight + document.querySelector('.footer-lrg').offsetHeight ) + 'px';
+    }
   } else if (document.querySelector('.info-container')) {
     document.querySelector('.info-container').style.minHeight = window.innerHeight - ( document.querySelector('.navbar-container').offsetHeight + document.querySelector('.footer-lrg').offsetHeight - 1 ) + 'px';
   }
